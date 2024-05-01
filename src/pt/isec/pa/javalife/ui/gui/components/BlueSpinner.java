b0c6f34341f42c6b40254f2d29ed16bce4544790
Rreis019 <rr199820031@gmail.com>
2024-05-01 17:53:14 +0100

    Fiz dois componentes BlueSlider e BlueSpinner ambos que vao ser usado no ecra criar ecossistema

---
+++ src/pt/isec/pa/javalife/ui/gui/components/BlueSpinner.java
@@ -0,0 +1,47 @@
+package pt.isec.pa.javalife.ui.gui.components;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+public class BlueSlider extends VBox {
+
+    private Slider slider;
+    private Label valueLabel;
+
+    public BlueSlider(String title, double width, double value,double max) {
+        this.slider = new Slider(0, max, value);
+        this.slider.setPrefWidth(width);
+        this.slider.setMaxWidth(width);
+
+        this.valueLabel = new Label(value + "/" + max);
+        this.valueLabel.getStyleClass().add("slider-text");
+
+        Label titleLabel = new Label(title);
+        titleLabel.getStyleClass().add("slider-text");
+
+        BorderPane pane = new BorderPane();
+        HBox.setHgrow(pane, Priority.ALWAYS);
+
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            valueLabel.setText(newValue.intValue() + "/" + (int)max);
+        });
+
+        HBox hbox = new HBox(titleLabel,pane,valueLabel);
+        hbox.setSpacing(14);
+
+        getChildren().addAll(hbox, slider);
+        setMaxWidth(width);
+    }
+
+    public Slider getSlider() {
+        return slider;
+    }
+
+    public Label getValueLabel() {
+        return valueLabel;
+    }
+}
