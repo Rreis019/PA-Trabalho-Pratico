5dcfce9b521ea00a956d236f782c5c9ec985376f
Rreis019 <rr199820031@gmail.com>
2024-05-07 19:38:31 +0100

    Centralizei o layout de criar ecossistema

--- src/pt/isec/pa/javalife/ui/gui/scenes/CreateEcosystemScene.java
+++ src/pt/isec/pa/javalife/ui/gui/scenes/CreateEcosystemScene.java
@@ -36,36 +36,38 @@ class CreateEcosystemScene extends Scene
         root.setPadding(new Insets(10));
         root.setSpacing(8);
 
+        VBox secondaryBackground = new VBox();
+        secondaryBackground.getStyleClass().add("secondary-background");
+        secondaryBackground.setAlignment(Pos.CENTER);
+        VBox.setVgrow(secondaryBackground, Priority.ALWAYS);
+
         //Titulo da pagina
         primaryStage.setTitle("JavaLife-CriarEcossistema");
 
         //Paa colocar tudo com a cor secundária de fundo
-        VBox secondaryVBox = new VBox();
-        secondaryVBox.getStyleClass().add("secondary-background");
-        secondaryVBox.setSpacing(10);
-        VBox.setVgrow(secondaryVBox, Priority.ALWAYS);
+        VBox content = new VBox();
+        content.getStyleClass().add("secondary-background");
+        content.setSpacing(10);
+        content.setMaxWidth(305);
 
         //Texto
         Label Texto = new Label("Criação do ecossistema");
         Texto.setWrapText(true);
-        Texto.setMaxWidth(300);
         Texto.setAlignment(Pos.CENTER_RIGHT);
         Texto.getStyleClass().add("heading");
-        Texto.setStyle("-fx-text-alignment: right;");
+        Texto.setStyle("-fx-text-alignment: left;");
 
 
         //Barra Branco
         Rectangle separate = new Rectangle();
-        separate.setWidth(190);
         separate.setHeight(2);
+        separate.setWidth(305);
         separate.setFill(Color.WHITE);
 
         // Campos de texto
         Label TextoNome = new Label("Nome");
-        TextoNome.setMaxWidth(300);
         TextoNome.setStyle("-fx-text-fill: white; -fx-text-alignment: left; -fx-font-size : 15px;");
         TextField Nome = new TextField();
-        Nome.setStyle("-fx-background-color: rgba(0,0,0,0.5);-fx-text-fill: white;");
 
         //Componentes
         BlueSpinner Slargura = new BlueSpinner("Largura",10,100,5);
@@ -85,22 +87,24 @@ class CreateEcosystemScene extends Scene
         HBox hbox1 = new HBox();
         hbox1.getChildren().addAll(Slargura, Scomprimento);
         hbox1.setSpacing(10); // Espaçamento entre os componentes
-        //hbox1.setAlignment(Pos.CENTER);
+        hbox1.setAlignment(Pos.CENTER);
 
         HBox hbox2 = new HBox();
         hbox2.getChildren().addAll(Sfauna, Sflora);
         hbox2.setSpacing(10); // Espaçamento entre os componentes
-        //hbox2.setAlignment(Pos.CENTER);
+        hbox2.setAlignment(Pos.CENTER);
+
+        HBox hbox3 = new HBox();
+        hbox3.getChildren().addAll(Sinanimados);
+
+
 
         Label Erro = new Label("Mensagem de Erro");
         Erro.setStyle("-fx-text-fill: white;-fx-font-size : 15px;");
 
-        //Adiciona tudo ao plano secundário
-        secondaryVBox.setAlignment(Pos.CENTER);
-        secondaryVBox.getChildren().addAll(Texto, separate, TextoNome,Nome, hbox1, hbox2,Sinanimados, Sunidade , Erro, criarButton);
-
-        //Adiciona o segundo plano ao plano principal
-        root.getChildren().addAll(secondaryVBox);
+        content.getChildren().addAll(Texto, separate, TextoNome,Nome, hbox1, hbox2,hbox3, Sunidade , Erro, criarButton);
+        secondaryBackground.getChildren().addAll(content);
+        root.getChildren().addAll(secondaryBackground);
 
         //Tamanho da janela
         primaryStage.setMinWidth(400);
